93db9cdb2d7f6a7e4be04884d8371eb989568e68
Marlus Saraiva <marlus.saraiva@gmail.com>
2020-06-15 15:03:48 -0300

    Add phoenix compiler

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule SurfaceBulma.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      compilers: [:phoenix] ++ Mix.compilers(),
       deps: deps()
     ]
   end
