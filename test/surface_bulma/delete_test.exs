0a454488211f75f86e235986354b58d73fd49728
Felipe Renan <feelipe.renan@gmail.com>
2020-06-18 23:24:14 +0300

    Add Delete component (#1)

---
+++ test/surface_bulma/delete_test.exs
@@ -0,0 +1,20 @@
+defmodule Surface.Components.DeleteTest do
+  use ExUnit.Case, async: true
+
+  alias SurfaceBulma.Delete, warn: false
+  import ComponentTestHelper
+
+  test "creates a link element with class delete" do
+    code = """
+    <Delete />
+    """
+    assert render_live(code) =~ ~s(<a class="delete"></a>)
+  end
+
+  test "property size" do
+    code = """
+    <Delete size="small"/>
+    """
+    assert render_live(code) =~ ~s(<a class="delete is-small"></a>)
+  end
+end
