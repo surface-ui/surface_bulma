53c0f6e0fa71f1a7a540fdac8cffeb300aeffa52
Jim Lemmers <jim.lemmers@gmail.com>
2020-12-08 18:50:27 +0100

    WIP: FontAwesome icons and Icon base module

---
+++ lib/surface_bulma/icon/font_awesome.ex
@@ -0,0 +1,71 @@
+defmodule SurfaceBulma.Icon.FontAwesome do
+  @moduledoc """
+  A component that displays an icon compatible with the FontAwesome icons
+  """
+
+  @icon_names :code.priv_dir(:surface_bulma)
+              |> Path.join("font-awesome-icons.txt")
+              |> File.read!()
+              |> String.split("\n")
+
+  @sizes ~w/xs sm lg 2x 3x 4x 5x 6x 7x 8x 9x 10x/
+  use SurfaceBulma.Icon,
+    icon_sizes: @sizes,
+    animations: ~w/spin pulse/,
+    icon_names: @icon_names
+
+  @doc """
+  Transformations of the icon. Use `rotate-#`, `grow-#`, `shrink-#`, `flip-v`, `flip-h`,
+  `right-#`, `left-#`, `up-#`, `down-#` in any combination. 
+  Rotations are in degrees, placement and scaling are in 1/16th `em`.
+  """
+  prop transform, :string
+
+  @doc "Sets the size for the stacking container, if stacking icons"
+  prop stack_size, :string, values: @sizes
+
+  @doc "Sets a mask to use with the icon"
+  prop mask, :string, values: @icon_names
+
+  @doc "Set the primary color of the icon"
+  prop primary_color, :string
+
+  @doc "set the secondary color of the icon"
+  prop secondary_color, :string
+
+  slot default
+
+  def render(assigns) do
+    stacking = slot_assigned?(:default)
+    [primary_color, secondary_color] = [assigns.primary_color, assigns.secondary_color]
+
+    style =
+      "#{primary_color && "--fa-primary-color: #{primary_color};"};" <>
+        "#{secondary_color && "--fa-secondary-color: #{secondary_color};"}"
+
+    transforms = (assigns.transform && %{"data-fa-transform": assigns.transform || ""}) || %{}
+
+    # The Context will tell the Components in the :default slot 
+    # whether to render a container span or not this allows for the stacking behaviour.
+
+    ~H"""
+    <Context get={{__MODULE__, container: container}}>
+      <span :if={{is_nil(container)}} class={{"icon", "is-#{@container_size}": @container_size}}>
+        <span :if={{stacking}} class={{"fa-stack", "fa-#{@stack_size}": @stack_size}} >
+          <i :attrs={{transforms}} style={{style}}
+            class={{"fas", "fa-#{@icon}", "fa-stack-#{@size}": @size}}></i>
+          <Context put={{__MODULE__, container: false}}>
+
+            <slot name="default" />
+          </Context>
+        </span>
+
+        <i :attrs={{transforms}} :if={{ not stacking }} class={{"fas", "fa-#{@icon}", "fa-#{@size}": @size}} style={{style}} ></i>
+      </span>
+
+      <i :attrs={{transforms}} :if={{not is_nil(container) and not container}} 
+        class={{"fas", "fa-#{@icon}", "fa-stack-#{@size}": @size}}></i>
+    </Context>
+    """
+  end
+end
---
+++ test/surface_bulma/icon_test.exs
@@ -0,0 +1,73 @@
+defmodule Surface.Components.IconTest do
+  use ExUnit.Case, async: true
+
+  alias SurfaceBulma.Icon.FontAwesome, as: FA, warn: false
+  import ComponentTestHelper
+
+  test "basic icon usage" do
+    code = """
+    <FA icon="500px" />
+    """
+
+    assert render_live(code) =~ """
+           <span class="icon"><i class="fas fa-500px"></i></span>
+           """
+  end
+
+  test "set container size" do
+    code = """
+    <FA icon="500px" container_size="small" />
+    """
+
+    assert render_live(code) =~ """
+           <span class="icon is-small"><i class="fas fa-500px"></i></span>
+           """
+  end
+
+  test "set icon size" do
+    code = """
+    <FA icon="500px" size="4x" container_size="small" />
+    """
+
+    assert render_live(code) =~
+             ~s[<span class="icon is-small"><i class="fas fa-500px fa-4x"></i></span>]
+  end
+
+  test "stacked icons" do
+    code = """
+    <FA icon="camera" container_size="large" stack_size="lg" size="1x">
+      <FA icon="ban" size="2x"/>
+    </FA>
+    """
+
+    assert render_live(code) =~
+             """
+             <span class="icon is-large">
+             <span class="fa-stack fa-lg"><i class="fas fa-camera fa-stack-1x"></i>
+             <i class="fas fa-ban fa-stack-2x"></i>
+             </span>
+             </span>
+             """
+             |> String.replace("\n", "")
+  end
+
+  test "transformations of the icon" do
+    code = """
+    <FA icon="500px" transform="grow-10 left-20"/>
+    """
+
+    assert render_live(code) =~ """
+           <span class="icon"><i data-fa-transform="grow-10 left-20" class="fas fa-500px"></i></span>
+           """
+  end
+
+  test "setting the primary color" do
+    code = """
+    <FA icon="camera" primary_color="blue" />
+    """
+
+    assert render_live(code) =~ """
+           <span class="icon"><i class="fas fa-camera" style="--fa-primary-color: blue"></i></span>
+           """
+  end
+end
