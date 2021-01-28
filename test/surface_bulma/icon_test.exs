797432243848d7e6cc70930522b641b1a7464724
Jim Lemmers <jim.lemmers@gmail.com>
2021-01-28 20:58:52 +0100

    switch to render_surface in tests

--- test/surface_bulma/icon_test.exs
+++ test/surface_bulma/icon_test.exs
@@ -1,48 +1,59 @@
 defmodule Surface.Components.IconTest do
-  use ExUnit.Case, async: true
+  use SurfaceBulma.ConnCase, async: true
 
-  alias SurfaceBulma.Icon.FontAwesome, as: FA, warn: false
-  alias SurfaceBulma.Icon.FontAwesome.{Layers, TextLayer, CounterLayer}, warn: false
-  import ComponentTestHelper
+  alias SurfaceBulma.Icon.FontAwesome, as: FA
+  alias SurfaceBulma.Icon.FontAwesome.{Layers, TextLayer, CounterLayer}
 
   test "basic icon usage" do
-    code = """
-    <FA icon="500px" />
-    """
+    html =
+      render_surface do
+        ~H"""
+        <FA icon="500px" />
+        """
+      end
 
-    assert render_live(code) =~ """
+    assert code =~ """
            <span class="icon"><i class="fas fa-500px"></i></span>
            """
   end
 
   test "set container size" do
-    code = """
-    <FA icon="500px" container_size="small" />
-    """
+    html =
+      render_surface do
+        ~H"""
+        <FA icon="500px" container_size="small" />
+        """
+      end
 
-    assert render_live(code) =~ """
+    assert code =~ """
            <span class="icon is-small"><i class="fas fa-500px"></i></span>
            """
   end
 
   test "set icon size" do
-    code = """
-    <FA icon="500px" size="4x" container_size="small" />
-    """
+    html =
+      render_surface do
+        ~H"""
+        <FA icon="500px" size="4x" container_size="small" />
+        """
+      end
 
-    assert render_live(code) =~
+    assert code =~
              ~s[<span class="icon is-small"><i class="fas fa-500px fa-4x"></i></span>]
   end
 
   test "Layered icons" do
-    code = """
-    <Layers container_size="large" >
-      <FA icon="camera" size="1x"/>
-      <FA icon="ban" size="2x"/>
-    </Layers>
-    """
+    html =
+      render_surface do
+        ~H"""
+        <Layers container_size="large" >
+          <FA icon="camera" size="1x"/>
+          <FA icon="ban" size="2x"/>
+        </Layers>
+        """
+      end
 
-    assert render_live(code) =~
+    assert code =~
              """
              <span class="icon is-large">
              <span class="fa-layers fa-fw">
@@ -55,31 +66,40 @@ defmodule Surface.Components.IconTest do
   end
 
   test "transformations of the icon" do
-    code = """
-    <FA icon="500px" transform="grow-10 left-20"/>
-    """
+    html =
+      render_surface do
+        ~H"""
+        <FA icon="500px" transform="grow-10 left-20"/>
+        """
+      end
 
-    assert render_live(code) =~ """
+    assert code =~ """
            <span class="icon"><i data-fa-transform="grow-10 left-20" class="fas fa-500px"></i></span>
            """
   end
 
   test "setting the primary color" do
-    code = """
-    <FA icon="camera" icon_style="duotone" primary_color="blue" />
-    """
+    html =
+      render_surface do
+        ~H"""
+        <FA icon="camera" icon_style="duotone" primary_color="blue" />
+        """
+      end
 
-    assert render_live(code) =~ """
+    assert code =~ """
            <span class="icon"><i class="fad fa-camera" style="--fa-primary-color: blue"></i></span>
            """
   end
 
   test "setting the primary opacity and secondary color" do
-    code = """
-    <FA icon="camera" icon_style="duotone" secondary_color="blue" primary_opacity={{0.6}} />
-    """
+    html =
+      render_surface do
+        ~H"""
+        <FA icon="camera" icon_style="duotone" secondary_color="blue" primary_opacity={{0.6}} />
+        """
+      end
 
-    assert render_live(code) =~ """
+    assert code =~ """
            <span class="icon"><i class="fad fa-camera" style="--fa-secondary-color: blue; --fa-primary-opacity: 0.6"></i></span>
            """
   end
