d66d996cb9c5d78a5f516ca46b92d60134975554
Aaron Graham <agraham19@gmail.com>
2021-05-29 16:50:15 -0700

    Fix compiler warning (#19)
    Width doesn't yet exist on Column

--- priv/catalogue/table/example01.ex
+++ priv/catalogue/table/example01.ex
@@ -20,7 +20,7 @@ defmodule SurfaceBulma.Catalogue.Table.Example01 do
   def render(assigns) do
     ~H"""
     <Table id="foo" data={{ person <- @props.data }} :props={{ @props }}>
-      <Column width="is-1" label="Id" sort_by="id">
+      <Column label="Id" sort_by="id">
         {{ person.id }}
       </Column>
       <Column label="Name" sort_by="name">
