747c525ad9114c8a857a777d8c852a8315f6aa83
Jim Lemmers <jim.lemmers@gmail.com>
2021-10-23 22:53:10 +0200

    Telephone input

---
+++ lib/surface_bulma/form/telephone_input.ex
@@ -0,0 +1,54 @@
+defmodule SurfaceBulma.Form.TelephoneInput do
+  @moduledoc """
+  The telephone field component as defined here: https://bulma.io/documentation/form/input/
+  """
+
+  use SurfaceBulma.Form.InputBase
+  import SurfaceBulma.Form.InputWrapper
+
+  alias Surface.Components.Form.TelephoneInput
+
+  include(TelephoneInput)
+  @doc "Max length of field, as enforced by client browser. Not validated by Elixir."
+  prop(maxlength, :integer)
+
+  @doc "Minimum length of field, as enforced by client browser. Not validated by Elixir."
+  prop(minlength, :integer)
+
+  def render(assigns) do
+    ~F"""
+      <SurfaceBulma.Form.InputWrapper :let={form: form}
+        field={@field}
+        label={@label}
+        expanded={@expanded}
+        help_text={@help_text}
+        disable_icons={@disable_icons}
+        icon_left={@icon_left}
+        icon_right={@icon_right}
+        field_class={@field_class}
+        has_addons={has_addons?(assigns)}>
+        <:left_addon>{render_left_addon(assigns)}</:left_addon>
+        <TelephoneInput
+        {...included_props(assigns)} 
+        class={[
+          "input",
+          "is-danger": has_error?(assigns),
+          "is-success": has_change?(assigns) && !has_error?(assigns),
+          "is-static": @static
+          ] ++ (@class || [])}
+        field={@field}
+        form={form || @form}
+        value={@value}
+        opts={
+          [
+            placeholder: @placeholder,
+            disabled: @disabled,
+            readonly: @readonly,
+            maxlength: @maxlength,
+            minlength: @minlength
+          ] ++ @opts}/>
+        <:right_addon>{render_right_addon(assigns)}</:right_addon>
+      </SurfaceBulma.Form.InputWrapper>
+    """
+  end
+end
--- test/surface_bulma/form/telephone_input_test.exs
+++ test/surface_bulma/form/telephone_input_test.exs
@@ -1,3 +1,15 @@
 defmodule SurfaceBulma.Components.Form.TelephoneInputTest do
   use SurfaceBulma.ConnCase
+
+  alias SurfaceBulma.Form.TelephoneInput
+
+  test "should use the correct type" do
+    html = render_surface do
+      ~F"""
+        <TelephoneInput form="user" field="tel" />
+      """
+    end
+
+    assert html =~ ~r/input .* type="tel"/
+  end
 end
