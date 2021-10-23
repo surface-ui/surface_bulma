950bab609c27e321f5053d29125291c11c9a2c14
Jim Lemmers <jim.lemmers@gmail.com>
2021-10-23 22:52:58 +0200

    Email input

--- test/surface_bulma/form/email_input_test.exs
+++ test/surface_bulma/form/email_input_test.exs
@@ -1,3 +1,14 @@
 defmodule SurfaceBulma.Components.Form.EmailInputTest do
   use SurfaceBulma.ConnCase
+  alias SurfaceBulma.Form.EmailInput
+  
+  test "email input is rendered properly" do
+    html = render_surface do
+      ~F"""
+      <EmailInput form="user" field="email" />
+      """
+      end
+
+      assert html =~ ~r/<input .* type="email"/
+  end
 end
