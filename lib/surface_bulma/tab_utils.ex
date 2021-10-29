78374e3490bc801e3fa6aa5a9e7cded3f4439e88
Jim Lemmers <jim.lemmers@gmail.com>
2021-10-29 11:49:46 +0200

    Abstract the tab clicking to TabUtils module for re-use in other modules

---
+++ lib/surface_bulma/tab_utils.ex
@@ -0,0 +1,95 @@
+defmodule SurfaceBulma.TabUtils do
+  @moduledoc false
+
+  # TODO make this dependent on the version of LiveView used. It would be preferable to use JS commands instead of a LiveComponent.
+  defmacro __using__(opts \\ []) do
+    moduledoc =
+      (Keyword.get(opts, :doc, "") <>
+        """
+
+        Tab selection is handled by an internal event when Phoenix LiveView version < 0.17.* is used.
+        A custom event can be specified for a tab, but be aware that the event handler has to send an update to the 
+        component containing the tab. 
+
+        For example given this markup:
+
+        ```jsx
+          ~F\"\"\"
+            <Panel id="mypanel">
+              <:title>Test Panel</:title>
+              <Tab title="first">
+                <TabItem>Item</TabItem>
+              </Tab>
+              <Tab title="special" click="my-event">
+                <TabItem>SpecialItem</TabItem>
+              </Tab>
+            </Panel>
+          \"\"\"
+        ```
+
+        The parent LiveView or LiveComponent needs to call `Panel.set_tab/2` to tell the `Panel` that the selected tab has changed.
+        The event handler would then look like this:
+        ```
+        def handle_event("my-event", %{"index" => index_str}, socket) do
+          ... do some other things ...
+          Panel.set_tab("mypanel", String.to_integer(index_str))
+        end
+
+        """)
+
+    quote do
+      @moduledoc unquote(moduledoc)
+      use Surface.LiveComponent
+
+      data active_tab, :integer
+
+      data set_active_tab, :integer
+
+      data animation, :string, default: ""
+
+      def update(%{set_active_tab: set_active_tab} = assigns, socket) do
+        socket =
+          socket
+          |> assign(assigns)
+          |> assign(:active_tab, set_active_tab)
+          |> assign(:set_active_tab, nil)
+
+        {:ok, socket}
+      end
+
+      def update(assigns, socket) do
+        socket =
+          socket
+          |> assign(assigns)
+          |> assign(:active_tab, Enum.find_index(assigns.tabs, & &1.visible))
+
+        {:ok, socket}
+      end
+
+      def set_tab(id, index) do
+        send_update(unquote(__CALLER__.module), id: id, set_active_tab: index)
+      end
+
+      def handle_event("tab_click", %{"index" => index_str}, socket) do
+        index = String.to_integer(index_str)
+        animation = next_animation(socket.assigns, index)
+        {:noreply, assign(socket, active_tab: index, animation: animation)}
+      end
+
+      defp next_animation(assigns, clicked_index) do
+        %{animation: animation, active_tab: active_tab} = assigns
+
+        cond do
+          clicked_index > active_tab ->
+            "slideInRight"
+
+          clicked_index < active_tab ->
+            "slideInLeft"
+
+          true ->
+            animation
+        end
+      end
+    end
+  end
+end
--- lib/surface_bulma/tabs.ex
+++ lib/surface_bulma/tabs.ex
@@ -2,8 +2,7 @@ defmodule SurfaceBulma.Tabs do
   @moduledoc """
   A simple horizontal navigation **tabs** component
   """
-
-  use Surface.LiveComponent
+  use SurfaceBulma.TabUtils
 
   @doc "Make tab full width"
   prop expanded, :boolean, default: false
@@ -13,21 +12,6 @@ defmodule SurfaceBulma.Tabs do
 
   @doc "The tabs to display"
   slot tabs, required: true
-
-  data active_tab, :integer, default: 0
-
-  data animation, :string, default: ""
-
-  def update(assigns, socket) do
-    first_visible_tab = Enum.find_index(assigns.tabs, & &1.visible)
-
-    socket =
-      socket
-      |> assign(assigns)
-      |> assign(:active_tab, first_visible_tab)
-
-    {:ok, socket}
-  end
 
   def render(assigns) do
     ~F"""
@@ -58,26 +42,5 @@ defmodule SurfaceBulma.Tabs do
       </section>
     </div>
     """
-  end
-
-  def handle_event("tab_click", %{"index" => index_str}, socket) do
-    index = String.to_integer(index_str)
-    animation = next_animation(socket.assigns, index)
-    {:noreply, assign(socket, active_tab: index, animation: animation)}
-  end
-
-  defp next_animation(assigns, clicked_index) do
-    %{animation: animation, active_tab: active_tab} = assigns
-
-    cond do
-      clicked_index > active_tab ->
-        "slideInRight"
-
-      clicked_index < active_tab ->
-        "slideInLeft"
-
-      true ->
-        animation
-    end
   end
 end
