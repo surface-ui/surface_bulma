b0262f673a1c0f33faceff0ec372fc08f3db3c59
Jim Lemmers <jim.lemmers@gmail.com>
2025-01-24 11:51:51 +0100

    update Link component

--- lib/surface_bulma/link.ex
+++ lib/surface_bulma/link.ex
@@ -1,53 +1,40 @@
 defmodule SurfaceBulma.Link do
   use SurfaceBulma.Component
-  alias Surface.Components.LivePatch
-  alias Surface.Components.LiveRedirect
-
-  include(LivePatch, except: [:to])
 
   prop patch, :string
   prop navigate, :string
   prop href, :string
 
+  @doc "Optional properties to pass to the Phoenix.Component.link component"
+  prop props, :map, default: %{}
+
+  prop opts, :keyword, default: []
+
   use SurfaceBulma.ColorProp
   use SurfaceBulma.ContextClass
-  
+
   @doc false
   data context_opts, :any, from_context: {SurfaceBulma.Link, :opts}
 
-  @doc false
-  data props, :map
-  
   slot default
 
   def render(assigns) do
     assigns =
       assign(assigns, :class, classes(assigns, "has-text-#{assigns[:color]}": assigns[:color]))
-      |> then(fn assigns ->
-        props = included_props(assigns, LivePatch)
-        {opts, props} = Map.pop(props, :opts, [])
-
-        assigns
-        |> assign(props: props, opts: opts)
-      end)
+      |> update(:opts, fn opts -> Keyword.merge(opts, assigns[:context_opts] || []) end)
 
     ~F"""
     {#case {@patch, @navigate}}
       {#match {patch, _} when is_binary(patch)}
-        <LivePatch to={patch} {...@props} {=@class} opts={Keyword.merge(@opts, @context_opts || [])}>
+        <.link {=patch} {...@props} {=@class} {=@opts}>
           <#slot />
-        </LivePatch>
+        </.link>
       {#match {_, redirect} when is_binary(redirect)}
-        <LiveRedirect
-          to={redirect}
-          {...@props}
-          {=@class}
-          opts={Keyword.merge(@opts, @context_opts || [])}
-        >
+        <.link navigate={redirect} {...@props} {=@class} {=@opts}>
           <#slot />
-        </LiveRedirect>
+        </.link>
       {#match _}
-        <a {=@href} {=@class}><#slot /></a>
+        <.link {=@href} {=@class} {=@opts}><#slot /></.link>
     {/case}
     """
   end
